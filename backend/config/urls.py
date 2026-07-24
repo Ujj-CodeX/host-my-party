77adab7b20f5e76f9cf9693e62712a6e7034659c
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-07-25 01:27:54 +0530

    Web socket fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('account.urls')),
     path('api/', include('party.urls')),
+    path('api/', include('order.urls')),
+    path('api/ai/', include('ai.urls')), 
+
 ]
